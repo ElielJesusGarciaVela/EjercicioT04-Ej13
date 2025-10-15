72853998d9348f3df55bc11465d137013b8425d6
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-15 18:16:18 +0200

    fix: finished exercise

--- src/App.java
+++ src/App.java
@@ -14,26 +14,37 @@ public class App {
         int v3 = sc.nextInt();
 
 
-        if (v1 <= v2 && v1 < v3){
-            if (v2 >= v1 && v2 < v3){
+        if (v3 > v2 && v3 > v1){
+            if (v2 > v1){
                 System.out.printf("""
                        Los números introducidos ordenados de menor a mayor son %d, %d, %d
                         """, v1,v2,v3);
             } else {
                 System.out.printf("""
                        Los números introducidos ordenados de menor a mayor son %d, %d, %d
-                        """, v1,v3,v2);
+                        """, v2,v1,v3);
             }
-        } else if ( v1 > v2 && v1 <= v3){
-            if (v1 > v3 && v2 < v3){
+        } else if ( v2 > v3 && v2 > v1){
+            if (v3 > v1){
                 System.out.printf("""
                        Los números introducidos ordenados de menor a mayor son %d, %d, %d
-                        """, v2,v3,v1);
+                        """, v1, v3, v2);
             } else {
                 System.out.printf("""
                        Los números introducidos ordenados de menor a mayor son %d, %d, %d
-                        """, v2,v1,v3);
+                        """, v3, v1, v2);
             }
+        } else if (v1 > v3 && v1 > v2){
+            if (v3 > v2){
+            System.out.printf("""
+                       Los números introducidos ordenados de menor a mayor son %d, %d, %d
+                        """, v2, v3, v1);
+             } else {
+                System.out.printf("""
+                       Los números introducidos ordenados de menor a mayor son %d, %d, %d
+                        """, v3, v2, v1);
+             }
         }
+        sc.close();
     }
 }
